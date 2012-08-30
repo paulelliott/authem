64910dbe6527206247785086702c17b08f32152b
Micah Cooper and Travis Anderson <travis+mrmicahcooper+syntaxritual@hashrocket.com>
2012-08-30 09:56:52 -0400

    Remove duplicate #signed_in?

--- lib/authem/controller_support.rb
+++ lib/authem/controller_support.rb
@@ -24,10 +24,6 @@ module Authem::ControllerSupport
     end
   end
 
-  def signed_in?
-    !!current_user
-  end
-
   def require_user
     unless current_user
       session[:return_to_url] = request.url
