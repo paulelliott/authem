309a9d4f12282776685c3b51ff2b303bb8693729
ingemar <ingemar@xox.se>
2013-02-14 16:33:41 -0500

    Include instance methods instead of extending them
    Proper use of ActiveSupport::Concern, fixes 
    problems with inheritance (i.e. calling super etc)

--- lib/authem/base_user.rb
+++ lib/authem/base_user.rb
@@ -10,36 +10,38 @@ module Authem::BaseUser
     validates_format_of :email, with: /^\S+@\S+$/
     validates_presence_of :password, on: :create
     validates_confirmation_of :password, message: 'should match confirmation'
+  end
 
-    def self.find_by_email(email)
+  module ClassMethods
+    def find_by_email(email)
       where("LOWER(email) = ?", email.downcase).first
     end
+  end
 
-    def remember_token
-      self[:remember_token] || generate_token(:remember)
-    end
+  def remember_token
+    self[:remember_token] || generate_token(:remember)
+  end
 
-    def session_token
-      self[:session_token] || generate_token(:session)
-    end
+  def session_token
+    self[:session_token] || generate_token(:session)
+  end
 
-    def reset_password(password, confirmation)
-      return false unless password.present?
+  def reset_password(password, confirmation)
+    return false unless password.present?
 
-      self.password = password
-      self.password_confirmation = confirmation
-      self.reset_password_token = nil
-      save
-    end
+    self.password = password
+    self.password_confirmation = confirmation
+    self.reset_password_token = nil
+    save
+  end
 
-    def reset_password_token!
-      generate_token(:reset_password)
-    end
+  def reset_password_token!
+    generate_token(:reset_password)
+  end
 
-    private
+  private
 
-    def generate_token(type)
-      Authem::Token.generate.tap { |token| update_column("#{type}_token", token) }
-    end
+  def generate_token(type)
+    Authem::Token.generate.tap { |token| update_column("#{type}_token", token) }
   end
 end
--- lib/authem/sorcery_user.rb
+++ lib/authem/sorcery_user.rb
@@ -6,16 +6,16 @@ module Authem::SorceryUser
     attr_accessor :password, :password_confirmation
 
     before_save :encrypt_password
+  end
 
-    def authenticate(password)
-      self if crypted_password.present? && ::BCrypt::Password.new(crypted_password) == [password, salt].join
-    end
+  def authenticate(password)
+    self if crypted_password.present? && ::BCrypt::Password.new(crypted_password) == [password, salt].join
+  end
 
-    def encrypt_password
-      if password.present?
-        self.salt = ::BCrypt::Engine.generate_salt
-        self.crypted_password = ::BCrypt::Password.create([password, salt].join)
-      end
+  def encrypt_password
+    if password.present?
+      self.salt = ::BCrypt::Engine.generate_salt
+      self.crypted_password = ::BCrypt::Password.create([password, salt].join)
     end
   end
 end
