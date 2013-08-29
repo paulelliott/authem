77aca63d5f7d5e08b1872facb044987e454d4cba
bthesorceror <bthesorceror@gmail.com>
2013-08-29 11:55:40 -0400

    Wraps User#authenticate to check for password

--- spec/authem/user_spec.rb
+++ spec/authem/user_spec.rb
@@ -3,6 +3,17 @@ require 'spec_helper'
 describe Authem::User do
   let(:user_class) { PrimaryStrategyUser }
   let!(:user) { PrimaryStrategyUser.create(email: 'someone@example.com', password: 'password', password_confirmation: 'password') }
+  let!(:passwordless_user) { PrimaryStrategyUser.new(email: 'someone@example.com') }
 
   it_should_behave_like 'base user'
+
+  describe '#authenticate' do
+    context 'no password present' do
+      subject { passwordless_user.authenticate(nil) }
+
+      it 'returns false' do
+        should be_false
+      end
+    end
+  end
 end
