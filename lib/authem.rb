f27a1572fe51598ddd58403b83b0d30a0bfec085
Pavel Pravosud <pavel@pravosud.com>
2013-12-25 18:46:59 -0500

    Align autoload in authem.rb

--- lib/authem.rb
+++ lib/authem.rb
@@ -1,11 +1,10 @@
 module Authem
-  autoload :BaseUser,    'authem/base_user'
-  autoload :User,        'authem/user'
-  autoload :SorceryUser, 'authem/sorcery_user'
-
-  autoload :Config, 'authem/config'
+  autoload :BaseUser,          'authem/base_user'
+  autoload :User,              'authem/user'
+  autoload :SorceryUser,       'authem/sorcery_user'
+  autoload :Config,            'authem/config'
   autoload :ControllerSupport, 'authem/controller_support'
-  autoload :Token, 'authem/token'
+  autoload :Token,             'authem/token'
 
   def self.configure(&block)
     Config.configure(&block)
