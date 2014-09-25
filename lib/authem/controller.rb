45ae313c4fba7aa49582064c175e1def3a7f8fdc
Joshua Davey <josh@joshuadavey.com>
2014-09-24 21:50:47 -0500

    Add Ruby 1.9.3 compatibility
    This adds compatibility for JRuby 1.7.x as well.

--- lib/authem/controller.rb
+++ lib/authem/controller.rb
@@ -8,29 +8,29 @@ module Authem
     included{ class_attribute :authem_roles }
 
     module SessionManagementMethods
-      def sign_in(model, **options)
+      def sign_in(model, options={})
         role = options.fetch(:as){ self.class.authem_role_for(model) }
         public_send "sign_in_#{role}", model, options
       end
 
-      def sign_out(model, **options)
+      def sign_out(model, options={})
         role = options.fetch(:as){ self.class.authem_role_for(model) }
         public_send "sign_out_#{role}"
       end
 
-      def clear_all_sessions_for(model, **options)
+      def clear_all_sessions_for(model, options={})
         role = options.fetch(:as){ self.class.authem_role_for(model) }
         public_send "clear_all_#{role}_sessions_for", model
       end
 
-      def redirect_back_or_to(url, **options)
+      def redirect_back_or_to(url, options={})
         url = session.delete(:return_to_url) || url
         redirect_to url, options
       end
     end
 
     module ClassMethods
-      def authem_for(role_name, **options)
+      def authem_for(role_name, options={})
         include SessionManagementMethods
         Authem::Role.new(self, role_name, options).setup!
       end
